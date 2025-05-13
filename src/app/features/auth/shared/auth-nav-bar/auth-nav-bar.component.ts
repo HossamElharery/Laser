721239a55fb1d9d5dd640f6e323ddd333b6edb22
HossamElharery <elharery7ossam@gmail.com>
2025-05-13 17:32:02 +0300

    d

---
+++ src/app/features/auth/shared/auth-nav-bar/auth-nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-nav-bar',
+  imports: [],
+  templateUrl: './auth-nav-bar.component.html',
+  styleUrl: './auth-nav-bar.component.scss'
+})
+export class AuthNavBarComponent {
+
+}
